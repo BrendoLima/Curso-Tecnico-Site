33839cc15098b1cbc2a30b8b7d2d89a77e2e54ea
Brendo Patusco Lima <47870425+BrendoLima@users.noreply.github.com>
2023-06-01 14:21:27 -0300

    14:21-01-06-23

---
+++ src/scripts/constructors/adjustIframeHeight.js
@@ -0,0 +1,23 @@
+function adjustIframeHeight(iframe) {
+    iframe.style.height =
+      iframe.contentWindow.document.body.scrollHeight + "px";
+    //
+    window.addEventListener("DOMContentLoaded", function () {
+      var elements = document.getElementsByTagName("iframe");
+      var propertyValue = "0";
+
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].setAttribute("frameborder", propertyValue);
+      }
+    });
+    //
+    window.addEventListener("DOMContentLoaded", function () {
+      var elements = document.getElementsByTagName("iframe");
+      var propertyValue = "no";
+
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].setAttribute("scrolling", propertyValue);
+      }
+    });
+    //
+  }
